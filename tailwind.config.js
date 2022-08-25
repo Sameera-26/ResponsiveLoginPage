58fa44f682a6103660139909542f434e6ecd51d8
Sameera Alam <alamsameera26@gmail.com>
2022-08-26 01:31:23 +0500

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+
+    
+    extend: {
+      colors:{
+        brightPurple:'#4b1ffb',
+        darkPurple: '#2c1ebe',
+        grayishBlue: '#edf4fb',
+        iconBlue: '#f3f7fe',
+        lightPurples: '#5f38fd',
+        darkGray: '#152559',
+        textGray:'#9c95c5',
+        createPurple: '#4017fd',
+      }
+    },
+  },
+  plugins: [],
+}
